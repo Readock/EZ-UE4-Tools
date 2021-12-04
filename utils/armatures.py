20ec7f3587366043dd1e823781e094014bd3d630
readock <readock@gmx.de>
2021-12-05 00:34:55 +0100

    fixes armature export

--- utils/armatures.py
+++ utils/armatures.py
@@ -1,10 +1,15 @@
 import bpy
 
 def get_actions(armature):
-    if not armature or not armature.animation_data or not armature.data.jk_adc:
+    if not armature or not armature.animation_data or not armature.data:
         return []
-    actions, _ = armature.data.jk_adc.get_actions(armature, False)
+    #actions, _ = armature.data.jk_adc.get_actions(armature, False)
+    actions = [act for act in bpy.data.actions if is_armature_using_action(armature, act)]
     return actions
+
+
+def is_armature_using_action(armature, action):
+    return True if any(fc.data_path.partition('"')[2].split('"')[0] in armature.data.bones for fc in action.fcurves) else False
 
 def create_animation_data(armature):
     if not armature.animation_data:
--- utils/modifiers.py
+++ utils/modifiers.py
@@ -8,4 +8,7 @@ def apply_modifiers(objectList):
         if obj.type == "MESH":
             objects.set_active(obj)
             for mod in obj.modifiers:
-                bpy.ops.object.modifier_apply(modifier=mod.name)
+                try:
+                    bpy.ops.object.modifier_apply(modifier=mod.name)
+                except Exception as ex: 
+                    print(f"WARNING: Could not apply modifier {mod.name} of {obj.name}")    
