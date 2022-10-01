736328646430fe2266bcd7b4f6bc7f6c9713876d
readock <readock@gmx.de>
2022-10-01 17:31:53 +0200

    fixes local view issue when not active window

--- utils/modes.py
+++ utils/modes.py
@@ -10,12 +10,19 @@ def switch_to_edit():
     if bpy.context.active_object and bpy.context.active_object.mode != 'EDIT':
         bpy.ops.object.mode_set(mode='EDIT')
 
+def is_in_local_view():
+    for area in bpy.context.screen.areas:
+        if area.type == 'VIEW_3D':
+            space = area.spaces[0]
+            return space.local_view
+    return False
+
 def switch_to_local_view():
     """ Switch view to local view """
-    if not bpy.context.space_data.local_view:
+    if not is_in_local_view():
         bpy.ops.view3d.localview()
 
 def switch_to_not_local_view():
     """ Switch view to not be in local view """
-    if bpy.context.space_data.local_view:
+    if is_in_local_view():
         bpy.ops.view3d.localview()
